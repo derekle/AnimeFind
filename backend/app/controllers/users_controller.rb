c61463adc97c4cb6ac61f2eae1963551ae99f15c
derek <derekle.creative@gmail.com>
2021-10-20 00:25:52 -0700

    add crud actions to users

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+    def index
+        puts 'fetching all users.'
+        users = User.all
+        render json: users, except:[:password_digest, :created_at, :updated_at]
+    end
+
+    def create
+        puts "creating user with the following params: #{params}"
+        u = User.create!(
+            username:params[:username], 
+            password:params[:password]
+            )
+            render json: u, except: [:password_digest, :created_at, :updated_at]
+    end
+
+    def show
+        render json: get_user, except: [:password_digest, :created_at, :updated_at]
+    end
+
+    def update
+        get_user.update!(
+            username:params[:username], 
+            password:params[:password]
+            )
+        render json: get_user, except: [:password_digest, :created_at, :updated_at]
+    end
+
+    def destroy
+        get_user.destroy
+    end
+
+    private
+    def get_user
+         User.find_by(id: params[:id]) || User.all    
+    end
 end
