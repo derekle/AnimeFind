186ed6c438dad2d1d9ba1b0cc0bae29015b67a10
derek <derekle.creative@gmail.com>
2021-10-20 00:39:48 -0700

    add a seed for users

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+i = 1
+while i < 5
+    u = []
+    u << User.create(username: "user_#{i}", password: 'test_password')
+    i = i + 1
+end
