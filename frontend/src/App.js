918514c4a2dc9d17678021fa7eac89fb57a95bed
derek <derekle.creative@gmail.com>
2021-10-20 03:53:03 -0700

    hooked up fetching users with redux-thunk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import { fetchUsers } from './actions/usersActions'
-import userList from './components/usersList.js'
+import UserList from './components/usersList.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    console.log('component mounted...')
+		this.props.fetchUsers();
+  }
+  
+  render() {
+    console.log('rendering...')
+    console.log(this.props)
+    return (
+      <div>
+        <UserList userNames={this.props.users} />
+      </div>
+    )
+  }
 }
-
-export default App;
+const mapStateToProps = (state) => {
+	return {
+		users: state.users,
+		loading: state.loading,
+	};
+};
+const mapDispatchToProps = (dispatch) => {
+	return {
+		fetchUsers: () => dispatch(fetchUsers()),
+	};
+};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
