e01a5c8942655d45dcfcf874cd84ab5aa31e6786
derek <derekle.creative@gmail.com>
2021-10-23 16:21:37 -0700

    create a store using redux;  darktheme w/ mui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TopNavBar from './components/topNavBar'
 
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
 import reportWebVitals from './reportWebVitals';
+import usersReducer from './reducers/usersReducer';
+
+
+const store = createStore(usersReducer, applyMiddleware(thunk));
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
   document.getElementById('root')
 );
 
