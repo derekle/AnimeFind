5e2de2b65ad47623b6c9bd1153896f8a70b542a6
derek <derekle.creative@gmail.com>
2021-10-23 16:26:16 -0700

    update users in current state

--- frontend/src/reducers/usersReducer.js
+++ frontend/src/reducers/usersReducer.js
@@ -1,11 +1,20 @@
 const usersReducer = (state = {users: [], loading: false}, action) => {
     switch (action.type) {
         case 'LOADING_USERS':
+            console.log(action.type)
             return {
                 ...state,
                 users: [...state.users],
                 loading: true
             }
+        case 'ADD_USERS':
+            console.log(action.type)
+			return {
+				...state,
+				users: action.users,
+				loading: false,
+			};
+        
         default:
             return state
     }
