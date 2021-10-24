58b4366715f3c98fe42d931d051152c7303e6abe
derek <derekle.creative@gmail.com>
2021-10-24 03:18:02 -0700

    generated anime resoure - moved from users

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_133127) do
+ActiveRecord::Schema.define(version: 2021_10_24_023007) do
+
+  create_table "animes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "username"
