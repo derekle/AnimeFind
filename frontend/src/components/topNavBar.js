a9a370410b3f3d802c55a59a15f7ebaa2fd8d904
derek <derekle.creative@gmail.com>
2021-10-24 21:35:12 -0700

    remove unused components, update menuicon

--- frontend/src/components/topNavBar.js
+++ frontend/src/components/topNavBar.js
@@ -1,19 +1,9 @@
 import '../css/topNavBar.css'
 import React, { Component } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
 
 export default class TopNavBar extends Component {
 
@@ -25,11 +15,12 @@ export default class TopNavBar extends Component {
 					<IconButton
 						size="large"
 						edge="start"
-						color="inherit"
 						aria-label="menu"
 						sx={{ mr: 2 }}
+						color='primary'
+						fontSize="medium" 
 					>
-						<AccountCircle />
+						<MenuIcon/>
 					</IconButton>
 				</div>
 				<div id='form'>
