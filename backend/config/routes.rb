09f8dad7df4731a8d2b2e209c17b966365973fdc
derek <derekle.creative@gmail.com>
2021-10-25 19:44:59 -0700

    add custom routes for fetching top 10

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :animes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  get '/animes/filter/top', :to => 'animes#top_index'
 end
