6ae24e59b4a70a7bd4d868eb01694072952be894
derek <derekle.creative@gmail.com>
2021-10-25 19:47:02 -0700

    added links to buttons in top nav bar

--- frontend/src/components/topNavBar.js
+++ frontend/src/components/topNavBar.js
@@ -3,9 +3,21 @@ import React, { Component } from 'react';
 
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import {withRouter} from 'react-router-dom'
 
 
-export default class TopNavBar extends Component {
+class TopNavBar extends Component {
+	constructor(props) {
+    super(props)
+
+    this.search = this.search.bind(this)
+	  }
+	
+	search() {
+		console.log('form clicked!!!!')
+		this.props.history.push('/Search')
+	}
 
 	render() {
 		return (
@@ -18,13 +30,15 @@ export default class TopNavBar extends Component {
 						aria-label="menu"
 						sx={{ mr: 2 }}
 						color='primary'
-						fontSize="medium" 
+						fontSize="medium"
+						component={Link}
+  						to='/Home'
 					>
 						<MenuIcon/>
 					</IconButton>
 				</div>
 				<div id='form'>
-					<form id='searchForm'>
+					<form id='searchForm' onClick={this.search}>
 								<input placeholder='search'></input>
 								<button type='submit'><img id='searchButton' src='https://upload.wikimedia.org/wikipedia/commons/7/7e/Vector_search_icon.svg' alt='MediaWiki authors, Public domain, via Wikimedia Commons'/></button>
 					</form>
@@ -33,3 +47,5 @@ export default class TopNavBar extends Component {
 		)
 	}
 }
+
+export default withRouter(TopNavBar)
