0447a64df7d2918b4d66c4a85ce6acd055d72ff2
derek <derekle.creative@gmail.com>
2021-10-25 19:57:40 -0700

    added home page view

---
+++ frontend/src/views/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import AnimeList from '../components/animeList'
+import '../css/home.css'
+
+const Home = props => {
+    console.log(props)
+    return(
+        <div className='main'>
+                <main className="parallaxMain">
+
+                    <section className="parallaxSection parallaxSection--header">
+                        <div className='headerContent'>
+                            <h1 className='headerText'>MY APP</h1>
+                            <p className='subText'>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps.</p>
+                        </div>
+                        <div className='gradient'/>
+                    </section>
+                    <section className="staticSection staticSection--base">
+                        <div className='shows'>
+                            <AnimeList header='Top 10 Anime' resource='/filter/top'  theme={props.theme} />
+                            <AnimeList header='All Anime' resource='/'  theme={props.theme}/>
+                        </div>
+                    </section>
+                    <section className='footerSection'>
+                        <div className='footerContent'>
+                        <h2 className='footerText'>FOOTER</h2>
+                        <p className='subText'>Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules.</p>
+                        </div>
+                    </section>
+                </main>
+        </div>
+    )
+}
+export default Home
