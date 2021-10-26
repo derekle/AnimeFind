3d775569551fca5d0c352d67f3b928e038e36ed5
derek <derekle.creative@gmail.com>
2021-10-25 19:44:43 -0700

    add custom fetching top 10 anime

--- backend/app/controllers/animes_controller.rb
+++ backend/app/controllers/animes_controller.rb
@@ -1,13 +1,28 @@
 require 'pry'
+require 'net/http'
+require 'uri'
+
 class AnimesController < ApplicationController
     def index
     puts 'fetching all anime.'
-    binding.pry
 
 
     qry = Jikan::Query.new
     railgun = qry.search("railgun", :anime)
 
     render json:  railgun.raw.fetch('results')
+    end
+
+    def show
+    end
+
+    def top_index
+        puts 'fetching top anime...'
+        source = 'https://api.jikan.moe/v3/top/anime'
+        resp = Net::HTTP.get_response(URI.parse(source))
+        data = resp.body
+        result = JSON.parse(data)
+
+        render json:  result.fetch('top').first(10)
     end
 end
