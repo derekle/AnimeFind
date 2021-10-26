e9ad3c812c709e13888bfddaaba17b138b15e2f7
derek <derekle.creative@gmail.com>
2021-10-25 19:57:51 -0700

    create search page view

---
+++ frontend/src/views/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from '../App';
+import AnimeList from '../components/animeList'
+import '../css/home.css'
+
+const Search = props => {
+    console.log(props)
+    return(
+        <div>Search</div>
+    )
+}
+export default Search
