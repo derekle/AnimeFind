f33fe2102c85f15b8c38d4ef3983f69af25e784b
derek <derekle.creative@gmail.com>
2021-10-28 11:36:32 -0700

    display search results on page

--- frontend/src/views/Search.js
+++ frontend/src/views/Search.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import App from '../App';
 import AnimeList from '../components/animeList'
-import '../css/home.css'
+import '../css/search.css'
+
+import { connect } from 'react-redux'
+
 
 const Search = props => {
     console.log(props)
     return(
-        <div>Search</div>
+        <div className='main'>
+            <main>
+                <section className="staticSection parallaxSection--header">
+                    <div className='headerContent'>
+                        <h1 className='headerText'>Search</h1>
+                    </div>
+                    <div className='gradient'/>
+                </section>
+                <section className="staticSection staticSection--base">
+                    <div className='shows'>
+                        <AnimeList header='Results' resource={'/search/'+(props.query.text||'')}  scroll='vertical' theme={props.theme}/>
+                    </div>
+                </section>
+
+            </main>
+            <section className='footerSection'>
+                <div className='footerContent'>
+                <h2 className='footerText'>FOOTER</h2>
+                <p className='subText'>Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules.</p>
+                </div>
+            </section>
+        </div>
     )
 }
-export default Search
+
+const mapStateToProps = (state) => {
+	return {
+        query: state.query
+	};
+};
+
+export default connect(mapStateToProps)(Search)
