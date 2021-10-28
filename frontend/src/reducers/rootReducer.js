c2fba143b82b028f4a2e181ac975ac6781159b07
derek <derekle.creative@gmail.com>
2021-10-28 11:37:57 -0700

    successfully combined reducers after reading docs

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -1,11 +1,34 @@
+const animeReducer = (state = {anime: [], loading: false}, action) => {
+    switch (action.type) {
+        case 'LOADING_ANIME':
+            console.log(action.type)
+            return {
+                ...state,
+                anime: [...state.anime],
+                loading: true
+            }
+        case 'ADD_ANIME':
+            console.log(action.type)
+			return {
+				...state,
+				anime: action.anime,
+				loading: false,
+			};
+        
+        default:
+            return state
+    }
+}
 
-import { combineReducers, createStore } from 'redux'
+export const searchReducer = (state = { query: ""}, action) => {
+    switch (action.type) {
+        case 'SUBMIT_SEARCH':
+            return {
+                ...state,
+                query: action.payload
+            }
+        default: return state;
+    }
+}
 
-import animeReducer from './animeReducer'
-import themeReducer from './themeReducer'
-
-const rootReducer = combineReducers({
-    anime: animeReducer,
-    theme: themeReducer
-})
-export default rootReducer
+export default animeReducer
