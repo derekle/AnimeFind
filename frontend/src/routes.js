04bfc75d8ab14081cda16206d18b7a1b495a3503
derek <derekle.creative@gmail.com>
2021-10-28 11:43:10 -0700

    add routing, default invalid url to home

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Home from './views/Home'
 import Search from './views/Search'
+import Browse from './views/Browse'
 
 import TopNavBar from './components/topNavBar'
 
@@ -17,9 +18,16 @@ const Routes = props => {
                     <Route exact path='/Home'>
                         <Home theme={props.theme}/>
                     </Route>
-                    <Route exact path='/Search'>
-                        <Search />
+                    <Route exact path='/'>
+                        <Redirect to='/Home'/>
                     </Route>
+                    <Route exact path='/Search'>
+                        <Search theme={props.theme}/>
+                    </Route>
+                    <Route exact path='/Browse'>
+                        <Browse />
+                    </Route>
+                    <Redirect to="/Home" />
                 </Switch>
             </BrowserRouter>
         </div>
