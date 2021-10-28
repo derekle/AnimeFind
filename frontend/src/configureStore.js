25d72c63f442049793a1d14de6531d3c4eeeea4f
derek <derekle.creative@gmail.com>
2021-10-28 11:40:53 -0700

    configured reducer in separate file

---
+++ frontend/src/configureStore.js
@@ -0,0 +1,7 @@
+import { applyMiddleware, createStore, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+
+import reducer from './reducers/reducers'
+
+const store = createStore(reducer, compose(applyMiddleware(thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+export default store
