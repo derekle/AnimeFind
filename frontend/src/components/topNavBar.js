a3c1057ff5b24c2829eb77d5048f65b49ba55eed
derek <derekle.creative@gmail.com>
2021-10-28 11:39:06 -0700

    componentalize searchbar

--- frontend/src/components/topNavBar.js
+++ frontend/src/components/topNavBar.js
@@ -5,25 +5,16 @@ import React, { Component } from 'react';
 
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from 'react-router-dom';
-import {withRouter} from 'react-router-dom'
-import { touchRippleClasses } from '@mui/material';
 
 import Sidebar from './sideBar'
+import Searchbar from './searchBar'
 
 class TopNavBar extends Component {
 	constructor(props) {
 		super(props)
-
-		this.search = this.search.bind(this)
 		this.state = {
 			sideBarVisible: false
 		}
-	}
-	
-	search() {
-		console.log('form clicked!!!!')
-		this.props.history.push('/Search')
 	}
 
 	toggleSidebar = () => this.setState(state => ({
@@ -52,10 +43,7 @@ class TopNavBar extends Component {
 						</IconButton>
 					</div>
 					<div id='form'>
-						<form id='searchForm' onClick={this.search}>
-									<input placeholder='search'></input>
-									<button type='submit'><img id='searchButton' src='https://upload.wikimedia.org/wikipedia/commons/7/7e/Vector_search_icon.svg' alt='MediaWiki authors, Public domain, via Wikimedia Commons'/></button>
-						</form>
+						<Searchbar />
 					</div>
 				</nav>
 				<Sidebar show={this.state.sideBarVisible}/>
@@ -64,4 +52,4 @@ class TopNavBar extends Component {
 	}
 }
 
-export default withRouter(TopNavBar)
+export default TopNavBar
