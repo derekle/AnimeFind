53865bb7b3a2ee7b4087b3c961e1ea4f84cc0390
derek <derekle.creative@gmail.com>
2021-10-28 11:39:27 -0700

    create working links in sidebar

--- frontend/src/components/sideBar.js
+++ frontend/src/components/sideBar.js
@@ -1,19 +1,53 @@
-import React from 'react'
-import '../css/sideBar.css'
+import React, { Component } from 'react';
 
-const Sidebar = (props) => {
-				
+
+import '../css/sideBar.css'
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import PageviewIcon from '@mui/icons-material/Pageview';
+
+import {withRouter} from 'react-router-dom'
+import {useHistory} from 'react-router-dom';
+
+
+export default function Sidebar(props) {
+    const history = useHistory();
+    const handleOnClick = (e) => {
+        console.log(e.target.id)
+        history.push(e.target.id)
+    }
+
     return (
         <div>
-        <div className={'visible-'+props.show.toString()+'-sidebar'}>
-            <p>test1</p>
-            <p>test2</p>
-            <p>test3</p>
+            <div className={'visible-'+props.show.toString()+'-sidebar'}>
+                <nav className='list'>
+                    <li id='/Home' className='listitem'  onClick={handleOnClick}>
+                        <div className='listicon'>
+                            <HomeIcon />
+                        </div>
+                        <div className='listlabel'>
+                            Home
+                        </div>
+                    </li>
+                    <li id='/Search' className='listitem'  onClick={handleOnClick}>
+                        <div className='listicon'>
+                            <SearchIcon />
+                        </div>
+                        <div className='listlabel'>
+                            Search
+                        </div>
+                    </li>
+                    <li id='/Browse' className='listitem'  onClick={handleOnClick}>
+                        <div className='listicon'>
+                            <PageviewIcon />
+                        </div>
+                        <div className='listlabel'>
+                            Browse
+                        </div>
+                    </li>
+                </nav>
             </div>
-            <div className={'visible-'+props.show.toString()+'-overlay'}/>
+                    <div id='Home' className={'visible-' + props.show.toString() + '-overlay'}/>
         </div>
-
     )
 }
-
-export default Sidebar
