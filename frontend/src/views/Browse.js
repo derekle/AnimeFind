37df34fa680455a9f63e5454062fb0e46909c43b
derek <derekle.creative@gmail.com>
2021-10-28 11:37:10 -0700

    create temporary browse page

---
+++ frontend/src/views/Browse.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from '../App';
+import AnimeList from '../components/animeList'
+import '../css/home.css'
+
+const Browse = props => {
+    console.log(props)
+    return(
+        <div>Browse</div>
+    )
+}
+export default Browse
