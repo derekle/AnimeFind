b94c37b5c5a386b253796374867f251b31d73f0f
derek <derekle.creative@gmail.com>
2021-11-02 02:54:56 -0700

    keep track of loading state, nest anime state

--- frontend/src/reducers/animeReducer.js
+++ frontend/src/reducers/animeReducer.js
@@ -1,11 +1,17 @@
-export default function anime(state = [], action){
+export default function anime(state = { loading: false, anime: [] }, action){
     switch (action.type) {
         case 'LOADING_ANIME':
             console.log(action.type)
-            return state
+            return state = {
+                    ...state,
+                    loading: action.loading
+            }
         case 'ADD_ANIME':
             console.log(action.type)
-        return state
+            return state = {
+                    ...state,
+                    anime: action.anime
+            }
         
         default:
             return state
