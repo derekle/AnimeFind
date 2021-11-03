31c1af47db1210addbbe45e3cf3050c8f7fe2c50
derek <derekle.creative@gmail.com>
2021-11-02 17:17:27 -0700

    code: parse the url params into api endpoint

--- frontend/src/js/math.js
+++ frontend/src/js/math.js
@@ -1,11 +1,21 @@
 // math - contains any function that just handles processing and manipulation of data outside of the components.
 
 // truncatestring - used to truncate the provided string to  provided maximum6 length.
-export default function truncateString(str, num) {
+export const truncateString = function(str, num) {
     if (str.length > num) {
         let subStr = str.substring(0, num)
         return subStr + '...'
     } else {
         return str
     }
+}
+
+// parseurl - used to bridge the parameters in a search url with params and the resource route in the api server
+export const  parseURL = function() {
+    let search = window.location.search;
+    let params = new URLSearchParams(search);
+
+    return params.get('genre')
+        ? '/search/genre/'+params.get('genre')
+        : '/search/'+params.get('q')
 }
