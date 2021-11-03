c9b835ee7927d4b63b8498a87ba8c72a3c21e77c
derek <derekle.creative@gmail.com>
2021-11-02 17:18:37 -0700

    fix: add clear function, dont mutate state

--- frontend/src/reducers/searchReducer.js
+++ frontend/src/reducers/searchReducer.js
@@ -1,8 +1,12 @@
-export default function query(state = '', action){
+const initialState = {text: '', url: ''}
+export default function query(state = initialState, action) {
     switch (action.type) {
+        case 'CLEAR_QUERY':
+            console.log(action.query)
+            return initialState
         case 'SEARCHING_QUERY':
             console.log(action.query)
-            return state = action.query        
+            return action.query
         default:
             return state
     }
