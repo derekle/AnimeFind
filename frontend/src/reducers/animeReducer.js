6525c91b7bb26758cdf18b02222e75c11ea84fb4
derek <derekle.creative@gmail.com>
2021-11-02 17:18:15 -0700

    fix: add clear function, don't mutate state

--- frontend/src/reducers/animeReducer.js
+++ frontend/src/reducers/animeReducer.js
@@ -1,18 +1,21 @@
-export default function anime(state = { loading: false, anime: [] }, action){
+const initialState = {loading: false, anime: []}
+export default function anime(state = initialState, action) {
     switch (action.type) {
         case 'LOADING_ANIME':
             console.log(action.type)
-            return state = {
+            return {
                     ...state,
                     loading: action.loading
             }
         case 'ADD_ANIME':
             console.log(action.type)
-            return state = {
+            return {
                     ...state,
                     anime: action.anime
             }
-        
+        case 'CLEAR_DATA':
+            console.log(action.type)
+            return initialState
         default:
             return state
     }
